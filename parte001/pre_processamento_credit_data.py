ae2a3be51fd5ca9eda8a4efee1aff7abf9537d67
Jaimerson <jaimersonsantos@ufam.edu.br>
2018-10-04 17:34:20 -0400

    Nada praticamente

--- parte001/pre_processamento_credit_data.py
+++ parte001/pre_processamento_credit_data.py
@@ -10,3 +10,15 @@ import pandas as pd
 base = pd.read_csv('credit-data.csv')
 base.describe()
 base.loc[base['age'] < 0]
+#apagar coluna
+base.drop('age', 1, inplace=True)
+#apagar apenas os registros com problemas
+base.drop(base[base.age < 0].index, inplace=True)
+#preencher manualmente os valores
+#preencher os valores com a média
+base.mean()
+base['age'].mean()
+base['age'][base.age > 0].mean()
+base.loc[base.age < 0, 'age'] = base['age'][base.age > 0].mean()
+pd.isnull(base['age'])
+base.loc[pd.isnull(base['age'])]
